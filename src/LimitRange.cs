dd93c2cbb672943b536e6f90f5f65687eb205b5e
LordofNotCommitting <shadymcshade247@gmail.com>
2025-10-27 17:47:30 -0400

    first commit

---
+++ src/LimitRange.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+namespace OptimizeQMRangeProcessing
+{
+    [HarmonyPatch(typeof(BallisticSystem), nameof(BallisticSystem.CreateBallistic))]
+    public class LimitRange
+    {
+        //steam mod ID 3594238447
+        //mod_updateworkshopitem 3594238447 C:\Users\Edgar\source\repos\OptimizeQMRangeProcessing\OptimizeQMRangeProcessing\src\bin\Debug\net48 thumbnail.png
+        static void Postfix(ref Ballistic __result)
+        {
+            //Plugin.Logger.Log("--- Range Optimizing applied");
+            if (__result.MaxDistance == -1)
+            {
+                __result.MaxDistance = 30;
+            }
+        }
+    }
+}
---
+++ src/ProjectileSpeedUp.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+namespace OptimizeQMRangeProcessing
+{
+    [HarmonyPatch(typeof(BallisticSystem), nameof(BallisticSystem.Next))]
+    public class ProjectileSpeedUp
+    {
+        public static void Prefix(MapGrid mapGrid, ref Ballistic ballistic, bool hitWall)
+        {
+            //Plugin.Logger.Log("--- Range Optimizing applied");
+            if (ballistic.PassedDistance > 10)
+            {
+                ballistic.PassTileDuration = 0.016666668f;
+            }
+        }
+    }
+
+
+
+}
